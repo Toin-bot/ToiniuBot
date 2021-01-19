23c570f9fa0f49d7e060d5bcda5e451884461a34
itachin77 <76402001+itachin77@users.noreply.github.com>
2021-01-18 22:27:08 -0300

    Update toinmenu.js

--- src/toinmenu.js
+++ src/toinmenu.js
@@ -1,5 +1,5 @@
-const toinmenu = (prefix, pushname) => {
-    return `◪ *Comandos do Toin*
+const itachimenu = (prefix, pushname) => {
+    return `◪ *Comandos do itachi*
     │
     ├─ ❏ ${prefix}setprefix
     ├─ ❏ ${prefix}block
@@ -9,4 +9,4 @@ const toinmenu = (prefix, pushname) => {
 
 }
 
-exports.toinmenu = toinmenu
+exports.itachimenu = itachimenu
